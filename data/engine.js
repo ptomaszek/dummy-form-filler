59669268694a6ff537124b465da261742fc202e9
Pawel Tomaszek <ptomaszek@users.noreply.github.com>
2014-03-30 00:13:52 +0000

    Dirty play.

--- data/engine.js
+++ data/engine.js
@@ -1,10 +1,81 @@
 function populateDummyData() {
-    var $inputs = findInputs();
-    var $textInputs = $inputs.filter(":text");
-    var $emailInputs = $inputs.filter("[type='email']");
+    var $where = $('form');
+    var $textInputs = findTextInputs($where);
+    var $emailInputs = findEmailInputs($where);
+    var $radioInputs = findRadioInputs($where);
+
+    console.log("Text inputs: %s", $textInputs.size());
+    console.log("Email inputs: %s", $emailInputs.size());
+    console.log("Radio inputs: %s", $radioInputs.size());
 
     populateTextInputs($textInputs);
     populateEmailInputs($emailInputs);
+    // populateRadioInputs($radioInputs);
+}
+
+/*Returns text inputs that are empty, visible and enabled. */
+function findTextInputs($here) {
+    var $inputs = $here.find('input:text');
+    var $fitleredInputs = $inputs.filter(function () {
+        if (isEmpty($(this)) && isVisible($(this)) && isEnabled($(this))) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    return $fitleredInputs;
+}
+
+/*Returns email inputs that are empty, visible and enabled. */
+function findEmailInputs($here) {
+    var $inputs = $here.find('input[type=email]');
+    var $fitleredInputs = $inputs.filter(function () {
+        if (isEmpty($(this)) && isVisible($(this)) && isEnabled($(this))) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    return $fitleredInputs;
+}
+
+/*Returns array of grouped (by name) radio inputs that are not set, are visible and enabled. */
+function findRadioInputs($here) {
+    var $inputs = $here.find('input:radio');
+    var $excludedInputNames = getUncheckedRadioInputGroups($inputs);
+
+    // alert($excludedInputNames.prop("name"));
+
+    var $fitleredInputs = $inputs.filter(function () {
+        if (isVisible($(this)) && isEnabled($(this))) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    return $fitleredInputs;
+}
+
+function getUncheckedRadioInputGroups($inputs) {
+    var alreadyCheckedRadioInputs = $inputs.filter(':checked').map(function (n, i) {
+        return $(i).prop('name');
+    });
+
+    var inputGroups = {};
+    $.each($inputs, function (key, value) {
+        var groupName = $(value).prop('name');
+        if ($.inArray(groupName, $(alreadyCheckedRadioInputs)) <0 ) { //if no radio in group set yet
+            if (!(groupName in inputGroups)) { //and no group for that radio exist 
+                inputGroups[groupName] = []; //then create an sub-array for a new radio group
+            }
+            inputGroups[groupName].push($(value));//add radio input to the group
+        }
+    });
+
+    return $inputGroups;
 }
 
 function populateTextInputs($inputs) {
@@ -19,6 +90,14 @@ function populateEmailInputs($inputs) {
         $(this).val(dummyEmail);
     });
 }
+
+/* function populateRadioInputs($inputs) {
+    //alert($inputs.size());
+    $inputs.each(function () {
+        alert();
+        $(this).checked(true);
+    });
+} */
 
 function getDummyText() {
     var startIndex = Math.floor(Math.random() * DEI_KOBOL.length - 5);
@@ -47,21 +126,6 @@ function getDummyEmail() {
     }
 
     return email += '.com';
-}
-
-/*Returns inputs that are empty, visible and enabled. */
-function findInputs() {
-    var $allInputs = $('form').find('input');
-
-    var $fitleredInputs = $allInputs.filter(function () {
-        if (isEmpty($(this)) && isVisible($(this)) && isEnabled($(this))) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-
-    return $fitleredInputs;
 }
 
 function isEmpty($input) {
