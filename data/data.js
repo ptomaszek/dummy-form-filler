a3164d434d14023e2e23f329643d5e4cc1c16a6e
Pawel Tomaszek <ptomaszek@users.noreply.github.com>
2014-03-30 14:06:20 +0100

    Checkbox support.

--- data/data.js
+++ data/data.js
@@ -1,5 +1,6 @@
 var DEI_KOBOL = 'Dei Kobol una apita uthoukarana '
 		+ 'Ukthea mavatha gaman kerimuta '
-		+ 'Obe satharane mua osavathamanabanta ' + 'Api obata yagnya karama';
+		+ 'Obe satharane mua osavathamanabanta ' + 'Api obata yagnya karama'
+		+ 'ph\'nglui mglw\'nafh Cthulhu R\'lyeh wgah\'nagl fhtagn';
 
 var LETTERS = "abcdefghijklmnopqrstuvwxyz";
--- data/engine.js
+++ data/engine.js
@@ -1,69 +1,69 @@
-var $HERE = $('form');
-
 function populateDummyData() {
-    $.each($HERE.find('input'), function () {
-        populateInputIfNotSetYet($(this));
+    var $here = $('form');
+    $.each($here.find('input'), function() {
+	populateInputIfNotSetYet($(this), $here);
     });
 }
 
-function populateInputIfNotSetYet($input) {
-	var dummyEmail = getDummyEmail();
-	
-    if ($input.is(':text')) {
-        if (isEmpty($input) && isVisible($input) && isEnabled($input)) {
-            $input.val(getDummyText());
-        }
+function populateInputIfNotSetYet($input, $topParent) {
+    var dummyEmail = getDummyEmail();
+
+    if ($input.is('[type=text]')) {
+	if (isEmpty($input) && isVisible($input) && isEnabled($input)) {
+	    $input.val(getDummyText());
+	}
     } else if ($input.is('[type=email]')) {
-        if (isEmpty($input) && isVisible($input) && isEnabled($input)) {
-            $input.val(dummyEmail);
-        }
-    } else if ($input.is(':radio')) {
-        var groupName = $input.prop('name');
-        var $radioInputs = findRadioInputsByName(groupName);
-        if (isVisible($input) && isEnabled($input) && !isRadioGroupSet($radioInputs)) {
-            checkRandomRadioButton($radioInputs);
-        }
+	if (isEmpty($input) && isVisible($input) && isEnabled($input)) {
+	    $input.val(dummyEmail);
+	}
+    } else if ($input.is('[type=radio]')) {
+	var groupName = $input.prop('name');
+	var $radioInputs = findInputsByTypeAndName($topParent, 'radio', groupName);
+	if (isVisible($input) && isEnabled($input) && !isAnyInputChecked($radioInputs)) {
+	    checkRandomInput($radioInputs);
+	}
+    } else if ($input.is('[type=checkbox]')) {
+	var groupName = $input.prop('name');
+	var $checkboxInputs = findInputsByTypeAndName($topParent, 'checkbox', groupName);
+	if (isVisible($input) && isEnabled($input) && !isAnyInputChecked($checkboxInputs)) {
+	    checkRandomInputs($checkboxInputs);
+	}
     }
 }
-function isRadioGroupSet($radioInputs){
+
+function isAnyInputChecked($inputs){
     var isGroupSet = false;
 
-    $($radioInputs.each(function() {
+    $inputs.each(function() {
         if($(this).is(':checked')){
             isGroupSet = true;
         }
-    }));
+    });
     
     return isGroupSet;
 }
 
-function findRadioInputsByName(name){
-    return $HERE.find("input:radio[name='" + name + "']");
+function findInputsByTypeAndName($here, type, name) {
+    return $here.find('input[type=' + type + '][name=' + name + ']');
 }
-function checkRandomRadioButton($radioInputs){
-   var randomIndex = Math.floor(Math.random()*$radioInputs.size());
-   $($radioInputs[randomIndex]).prop('checked', true);
+function checkRandomInput($inputs) {
+    var randomIndex = Math.floor(Math.random() * $inputs.size());
+    $($inputs[randomIndex]).prop('checked', true);
 }
 
-/*Returns email inputs that are empty, visible and enabled. */
-function findEmailInputs($here) {
-    var $inputs = $here.find('');
-    var $fitleredInputs = $inputs.filter(function () {
-        if (isEmpty($(this)) && isVisible($(this)) && isEnabled($(this))) {
-            return true;
-        } else {
-            return false;
-        }
+function checkRandomInputs($inputs) {
+    var randomBoolean;
+    $.each($inputs, function() {
+	randomBoolean = (Math.floor(Math.random() * 2) === 0);
+	$($(this)).prop('checked', randomBoolean);
     });
-
-    return $fitleredInputs;
 }
 
 function getDummyText() {
     var startIndex = Math.floor(Math.random() * DEI_KOBOL.length - 5);
 
     if (startIndex < 0) {
-        startIndex = 0;
+	startIndex = 0;
     }
 
     var endIndex = startIndex + 5 + Math.floor(Math.random() * 10);
@@ -76,13 +76,13 @@ function getDummyEmail() {
     var email = "";
     var size = 3 + Math.floor(Math.random() * 7);
 
-    for (var i = 0; i < size; i++) {
-        email += LETTERS.charAt(Math.floor(Math.random() * LETTERS.length));
+    for ( var i = 0; i < size; i++) {
+	email += LETTERS.charAt(Math.floor(Math.random() * LETTERS.length));
     }
 
     email += '@'
-    for (var i = 0; i < 5; i++) {
-        email += LETTERS.charAt(Math.floor(Math.random() * LETTERS.length));
+    for ( var i = 0; i < 5; i++) {
+	email += LETTERS.charAt(Math.floor(Math.random() * LETTERS.length));
     }
 
     return email += '.com';
