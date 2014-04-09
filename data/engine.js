3a74f7b5f88bd3b7c3db0a084c53cc82c9f210d4
Pawel Tomaszek <ptomaszek@users.noreply.github.com>
2014-04-09 23:16:04 +0100

    Some kind of semantic check, e.g. text field with a label 'Phone' will be populated with dummy phone number rather than dummy text.

--- data/engine.js
+++ data/engine.js
@@ -7,11 +7,15 @@ function populateDummyData() {
 
 var dummyEmail = getDummyEmail();
 
+var PHONE_INPUT = 'phone_input';
+var AGE_INPUT = 'age_input';
+var YEAR_INPUT = 'year_input';
+
 function populateInputIfNotSetYet($input, $topParent) {
 
     if ($input.is('[type=text]')) {
 	if (isEmpty($input) && isVisible($input) && isEnabled($input)) {
-	    $input.val(getDummyText());
+	    populateWithRandomTextWisely($input);
 	}
     } else if ($input.is('[type=email]')) {
 	if (isEmpty($input) && isVisible($input) && isEnabled($input)) {
@@ -33,6 +37,68 @@ function populateInputIfNotSetYet($input, $topParent) {
 	if (isEmpty($input) && isVisible($input) && isEnabled($input)) {
 	    populateWithRandomNumberWisely($input);
 	}
+    }
+}
+
+/*
+ * Considers: 
+ * - min and max properties 
+ * - name and label to guess input's role, e.g. age, year
+ */
+function populateWithRandomTextWisely($input) {
+    var inputPurpose = defineInputPurpose($input);
+
+    switch (inputPurpose) {
+    case PHONE_INPUT:
+	$input.val(getDummyPhone());
+	break;
+    case AGE_INPUT:
+	$input.val('32');
+	break;
+    case YEAR_INPUT:
+	$input.val('1972');
+	break;
+    default:
+	$input.val(getDummyText());
+    }
+}
+
+/*
+ * Considers: 
+ * - min and max properties 
+ * - name and label to guess input's role, e.g. age, year
+ */
+function defineInputPurpose($input) {
+    var purposeByLabel = defineInputPurposeByLabel($input);
+
+    if (typeof purposeByLabel !== "undefined") {
+	return purposeByLabel;
+    }
+}
+
+function containsText(searchFor, inString) {
+    return inString.toLowerCase().indexOf(searchFor) >= 0;
+}
+
+/*
+ * Considers label text: 
+ * - phone
+ * - age
+ * - year
+ */
+function defineInputPurposeByLabel($input) {
+    var labelText = '';
+
+    if ($input.prop('id')) {
+	labelText = $('label[for="' + $input.prop('id') + '"]').text();
+    }
+
+    if (containsText('phone', labelText)) {
+	return PHONE_INPUT;
+    } else if (containsText('age', labelText)) {
+	return AGE_INPUT;
+    } else if (containsText('year', labelText)) {
+	return YEAR_INPUT;
     }
 }
 
@@ -98,6 +164,10 @@ function getDummyText() {
     return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
+function getDummyPhone() {
+    return '123456789';
+}
+
 function getDummyEmail() {
     var email = "";
     var random3To9 = 3 + Math.floor(Math.random() * 7);
@@ -130,4 +200,8 @@ function isVisible($input) {
 
 function isEnabled($input) {
     return $input.is(":enabled");
+}
+
+$.fn.exists = function() {
+    return this.length !== 0;
 }
