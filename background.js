5ee185d3bafcb485a8ee55cd245677adc71e4f76
TBD <TBD>
2017-04-13 18:50:24 +0200

    key changed

--- background.js
+++ background.js
@@ -1,20 +1,10 @@
 chrome.browserAction.onClicked.addListener(() => {
-      run();
+      chrome.storage.local.get(
+                  DEFAULT_OPTIONS,
+                  (options) => {
+                      chrome.storage.local.set(
+                          options, chrome.tabs.executeScript({file: "js/main.js"})
+                      );
+                  }
+              );
 });
-
-chrome.commands.onCommand.addListener(command => {
-  if (command == "run-dummy-run") {
-      run();
-  }
-});
-
-run = function () {
-    chrome.storage.local.get(
-            DEFAULT_OPTIONS,
-            function (options) {
-                chrome.storage.local.set(
-                    options, chrome.tabs.executeScript({file: "js/main.js"})
-                 );
-            }
-        );
-};
